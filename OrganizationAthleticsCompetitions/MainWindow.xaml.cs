dc75431683576e981bf7a246e70af21b9d2ba9c3
Nasurtdinova <nasurtdinovag@mail.ru>
2022-09-09 20:47:07 +0300

    Made design in main page

--- OrganizationAthleticsCompetitions/MainWindow.xaml.cs
+++ OrganizationAthleticsCompetitions/MainWindow.xaml.cs
@@ -12,17 +12,127 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace OrganizationAthleticsCompetitions
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer;
+
+        double panelWidth;
+        bool hidden;
+
         public MainWindow()
         {
             InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(1);
+            timer.Tick += Timer_Tick;
+
+            panelWidth = sidePanel.Width;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (hidden)
+            {
+                mainIcon.Visibility = Visibility.Visible;
+                sidePanel.Width += 1;
+                if (sidePanel.Width >= panelWidth)
+                {
+                    timer.Stop();
+                    hidden = false;
+                    lbRoleName.Visibility = Visibility.Visible;
+                    lbWelcome.Visibility = Visibility.Visible;
+                    mainFrame.Margin = new Thickness(210, 50, 0, 0);
+                    panelHeader.Margin = new Thickness(210, 0, 0, 0);
+                }
+            }
+            else
+            {
+                mainIcon.Visibility = Visibility.Hidden;
+                sidePanel.Width -= 1;
+                if (sidePanel.Width <= 45)
+                {
+                    timer.Stop();
+                    hidden = true;
+                    lbRoleName.Visibility = Visibility.Collapsed;
+                    lbWelcome.Visibility = Visibility.Collapsed;
+                    mainFrame.Margin = new Thickness(40, 50, 0, 0);
+                    panelHeader.Margin = new Thickness(40, 0, 0, 0);
+                }
+            }
+        }
+
+        private void btnMenu_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void btnExport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void sportsmanAdmin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void sportsman_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnHome_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void competition_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void competitionAdmin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void command_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void commandAdmin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void resultCompetition_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void resultCompetitionAdmin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnProfile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
